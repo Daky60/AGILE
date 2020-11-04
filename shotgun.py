aa2db3ba9edcbb58ff3690712ed1dfad317b1dd3
Daky <mail@sebfrodin.se>
2020-11-04 20:52:57 +0100

    Create shotgun.py

---
+++ shotgun.py
@@ -0,0 +1,8 @@
+class player:
+    def __init__(self):
+        self.action = 0
+        self.ammo = 0
+    def set_action(self, action):
+        self.action = action
+    def set_ammo(self, amount):
+        self.ammo = amount
